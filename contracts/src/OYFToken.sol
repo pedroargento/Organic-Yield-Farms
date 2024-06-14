2920784a837d45b62250db27d716dacad0c415da
Pedro Argento <pedro.argento@gmail.com>
2024-06-13 23:19:32 -0300

    end auction handler

--- contracts/src/OYFToken.sol
+++ contracts/src/OYFToken.sol
@@ -5,10 +5,10 @@ import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AccessControlERC20Mint is ERC20, AccessControl, Ownable {
+contract OYFToken is ERC20, AccessControl, Ownable {
     bytes32 public constant DAPP_ROLE = keccak256("DAPP_ROLE");
 
-    constructor(uint256 initialSupply) ERC20("MyToken", "TKN") Ownable(msg.sender) {
+    constructor(uint256 initialSupply) ERC20("OYFToken", "OYF") Ownable(msg.sender) {
       _mint(msg.sender, initialSupply);
     }
     
@@ -17,13 +17,18 @@ contract AccessControlERC20Mint is ERC20, AccessControl, Ownable {
         renounceOwnership();
     }
 
-    function mintAfterTimestamp(address to, uint256 amount, uint256 ts) public onlyRole(DAPP_ROLE) {
-        require(block.timestamp >= ts);
-        _mint(to, amount);
+    function mintAfterTimestamp(address _to, uint256 _amount, uint256 _ts) public onlyRole(DAPP_ROLE) {
+        require(block.timestamp >= _ts);
+        _mint(_to, _amount);
     }
 
-    function burnAfterTimestamp(address from, uint256 amount, uint256 ts) public onlyRole(DAPP_ROLE) {
-        require(block.timestamp >= ts);
-        _burn(from, amount);
+    function burnAfterTimestamp(address _from, uint256 _amount, uint256 _ts) public onlyRole(DAPP_ROLE) {
+        require(block.timestamp >= _ts);
+        _burn(_from, _amount);
+    }
+
+    function transferAfterTimestamp(address _to, uint256 _amount, uint256 _ts) public {
+        require(block.timestamp >= _ts);
+        transfer(_to, _amount);
     }
 }
--- dapp.py
+++ dapp.py
@@ -1,16 +1,20 @@
 import json
 import logging
-from typing import Dict
 
-from cartesi import DApp, JSONRouter, Rollup, RollupData, ABIRouter
+from cartesi import ABIRouter, DApp, JSONRouter, Rollup, RollupData, abi
 from cartesi.router import DAppAddressRouter
+from cartesi.vouchers import create_voucher_from_model
+from pydantic import BaseModel
 
-from auction import *
+from auction import (Auction, Operation, aggregate_vouchers, auction_output,
+                     auction_price, auction_vouchers)
 
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 ADDRESS_RELAY_ADDRESS = "0xf5de34d6bbc0446e2a45719e718efebaae179dae"
+ERC20_PORTAL = "0x9C21AEb2093C32DDbC53eEF24B873BDCd1aDa1DB"
+TOKEN_ADDRESS = "0x"
 
 dapp = DApp()
 json_router = JSONRouter()
@@ -21,7 +25,6 @@ dapp.add_router(json_router)
 dapp.add_router(dapp_address)
 dapp.add_router(abi_router)
 
-ERC20_PORTAL = "0x9C21AEb2093C32DDbC53eEF24B873BDCd1aDa1DB"
 # This dapp will read and write from this global state dict
 state: dict[str, Auction] = {}
 
@@ -49,10 +52,10 @@ def to_jsonhex(data):
 def handle_new_auction(rollup: Rollup, data: RollupData):
     metadata = data["metadata"]
     auction_data = data.json_payload()
-    key = auction_ata["id"]
+    key = auction_data["id"]
     state[key] = Auction(
         int(auction_data["end_time"] + metadata["timestamp"]),
-        int(auction_data["lock_time"] + metadata["timestamp"]),
+        int(auction_data["lock_time"]),
         int(auction_data["volume_limit"]),
         int(auction_data["reserve_price"]),
         [],
@@ -60,28 +63,77 @@ def handle_new_auction(rollup: Rollup, data: RollupData):
 
     rollup.report(to_jsonhex(state))
     return True
+
+
+class TransferArgs(BaseModel):
+    _to: abi.Address
+    _amount: abi.UInt256
+    _ts: abi.UInt256
+
+
+class MintArgs(BaseModel):
+    _to: abi.Address
+    _amount: abi.UInt256
+    _ts: abi.UInt256
+
+
+class BurnArgs(BaseModel):
+    _from: abi.Address
+    _amount: abi.UInt256
+    _ts: abi.UInt256
+
+
 # Example Json:
 # {
 #    "op": "end-auction",
 #    "id": "key_1",
 # }
 @json_router.advance({"op": "end-auction"})
-def handle_new_auction(rollup: Rollup, data: RollupData):
+def handle_end_auction(rollup: Rollup, data: RollupData):
+    if dapp_address.address is None:
+        return False
     metadata = data["metadata"]
     auction_data = data.json_payload()
-    key = dauction_ata["id"]
-    auction=state[key]
+    key = auction_data["id"]
+    auction = state[key]
 
     output = auction_output(auction.bids, auction.volume_limit)
     price = auction_price(output)
     vouchers = auction_vouchers(output.bid_outputs, price)
     aggregated_vouchers = aggregate_vouchers(vouchers)
-    rollup.report(to_jsonhex(state))
+    for v in aggregated_vouchers:
+        lock_timestamp = metadata["timestamp"] + auction.lock_time * v.timestamp_locked
+        if v.op == Operation.BURN:
+            voucher = create_voucher_from_model(
+                destination=TOKEN_ADDRESS,
+                function_name="burnAfterTimestamp",
+                args_model=BurnArgs(
+                    _from=dapp_address.address, _amount=v.amount, _ts=lock_timestamp
+                ),
+            )
+        if v.op == Operation.MINT:
+            voucher = create_voucher_from_model(
+                destination=TOKEN_ADDRESS,
+                function_name="mintAfterTimestamp",
+                args_model=MintArgs(_to=v.user, _amount=v.amount, _ts=lock_timestamp),
+            )
+        if v.op == Operation.TRANSFER:
+            voucher = create_voucher_from_model(
+                destination=TOKEN_ADDRESS,
+                function_name="transferAfterTimestamp",
+                args_model=TransferArgs(
+                    _to=v.user, _amount=v.amount, _ts=lock_timestamp
+                ),
+            )
+        rollup.voucher(voucher)
+
     return True
+
 
 @abi_router.advance(msg_sender=ERC20_PORTAL)
 def new_bid(rollup: Rollup, data: RollupData):
     pass
+
 
 @json_router.inspect({"op": "get"})
 def handle_inspect_get(rollup: Rollup, data: RollupData):
